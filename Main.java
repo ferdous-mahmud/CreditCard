a49697c7b88bfd5c1dc4ea432040051af8bcb5c6
ferdous-webdev <ferdous.webdev@gmail.com>
2020-11-18 09:21:02 +0600

    customer info display done

--- Main.java
+++ Main.java
@@ -3,11 +3,14 @@ import java.util.Scanner;
 public class Main{
     public static void main(String[] args){
 
-        //creating object for CreditCard class
-        CreditCard obj = new CreditCard("Ferdous","Brack Bank","CSE-1190320404",10000,1000.50);
+        //creating object 
+        CreditCard obj = new CreditCard("Mr. Rahim","Prime Bank Ltd.","5391037593875309",2500,3000);
+        CreditCard obj2 = new CreditCard("Mr. Karim","Brack Bank Ltd.","3485039933951954",3500);
+        CreditCard obj3 = new CreditCard("Mrs. Lipi","Prime Bank Ltd.","6391037593875309");
 
-        obj.getCustomer();
         obj.show();
+
+        
     }
 }
 // Credit card class
@@ -16,29 +19,62 @@ class CreditCard{
     String customer , bank , account ;
     int limit ;
     double balance;
-    String cname;
 
     //constructor
-    CreditCard(String cname ,String cbank , String caccount , int clim ,double cbalance){
+    public CreditCard(String cname ,String cbank , String caccount , int clim ,double cbalance){
         customer = cname;
         bank = cbank;
         account = caccount;
         limit = clim;
         balance = cbalance;
     }
-    
+    //constructor 2
+    public CreditCard(String cname ,String cbank , String caccount , int clim ){
+        customer = cname;
+        bank = cbank;
+        account = caccount;
+        limit = clim;
+    }
+    //constructor 3
+    public CreditCard(String cname ,String cbank , String caccount ){
+        customer = cname;
+        bank = cbank;
+        account = caccount;
+    }
     // scanner for input data
     Scanner input = new Scanner(System.in);
 
     // method for get customer name
-    String getCustomer(){  
-        cname = input.nextLine();
-        return cname;
+    public  void getCustomer(String customer){  
+        customer = input.nextLine();
+    }
+
+
+    //update methods
+    //charge method
+    public boolean charge(double price){
+        if(price + balance > limit)
+            return false;
+            balance += price;
+        return true;
+    }
+    //makePayment method
+    public void makePayment(double amount){
+        balance -= amount;
     }
 
     //method for showing everithing
-    void show(){
-        System.out.println("Customer Name : "+cname);
-    }
+    public void show(){
+        //showing account info
+        System.out.println();
+        System.out.println("                           Welcome!");
+        System.out.println("+-------------------+--------------------+----------------------+");
+        System.out.println("|   Customer Name   |   Bank Name        |   Account No         |");
+        System.out.println("+-------------------+--------------------+----------------------+");
+        System.out.println("|   "+customer+"       |   "+bank+"  |   "+account+"   |");
+        System.out.println("+-------------------+--------------------+----------------------+");
+        System.out.println();
 
+        charge(100.9);
+    }
 }
